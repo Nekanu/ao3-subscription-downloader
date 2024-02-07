eaacebbf2af0d4985e8242e4572af0f3c74fa3dd
Joshua Nestler <development@nekanu.com>
2024-02-07 14:50:33 +0100

    Better formatting for stdout

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from colorama import Fore, Style
 from slugify import slugify
 from ebooklib import epub
+from datetime import datetime, timedelta
 
 BOOK_FORMAT = "EPUB"
 TARGET_DIR = os.path.join(os.path.curdir, "books")
@@ -15,13 +16,16 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
+    i = 0
+
     for subscription in session.get_work_subscriptions():
 
         work = AO3.Work(subscription.id, session=session, load_chapters=False)
+        title = f"{Fore.CYAN}{work.authors[0].username} - {work.title}".ljust(80)
 
         # Skip the download if the work is restricted
         if work.restricted:
-            print(f"{Fore.YELLOW}Download failed: {work.title} - RESTRICTED{Style.RESET_ALL}")
+            print(f"{title}\t{Fore.YELLOW}SKIPPED - RESTRICTED{Style.RESET_ALL}")
             continue
 
         filename = f"{work.authors[0].username}_{slugify(work.title)}.{BOOK_FORMAT.lower()}"
@@ -29,16 +33,15 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
 
         # Check if the file already exists in the EBOKS_PATH with the same number of chapters
         if os.path.exists(os.path.join(target_dir, filename)):
-            existing_work_filepath = os.path.join(target_dir, filename)
-            existing_work = epub.read_epub(existing_work_filepath)
-
-            # If the file has the same number of chapters or less, skip the download
-            # (the -3 is to account for the 3 extra chapters in the epub file created by AO3)
-            if work.nchapters <= len(existing_work.spine) - 3:
-                print(f"{Fore.GREEN}Download skipped: {work.title} - UP TO DATE{Style.RESET_ALL}")
-                continue
-
-        print(f"{Fore.BLUE}Downloading: {Fore.CYAN}{work.authors[0].username} - {work.title}{Style.RESET_ALL}\r")
+            try:
+                existing_work = epub.read_epub(os.path.join(target_dir, filename))
+                # If the file has the same number of chapters or less, skip the download
+                # (the -3 is to account for the 3 extra chapters in the epub file created by AO3)
+                if work.nchapters <= len(existing_work.spine) - 3:
+                    print(f"{title}\t{Fore.GREEN}SKIPPED - UP TO DATE{Style.RESET_ALL}")
+                    continue
+            except Exception as e:
+                print(f"{Fore.RED}Error reading existing file: {filename}\t\t{Style.DIM}{e.args[0]}{Style.RESET_ALL}")
 
         try:
             # Download the work
@@ -46,17 +49,18 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
             
             # If the file is empty, download failed
             if os.path.getsize(os.path.join(target_dir, filename_tmp)) == 0:
-                print(f"{Fore.RED}Download failed: {work.title} - EMPTY{Style.RESET_ALL}")
+                print(f"{title}\t{Fore.RED}FAILED - EMPTY FILE{Style.RESET_ALL}")
                 os.remove(os.path.join(target_dir, filename_tmp))
                 continue
             
             # If the file is not empty, rename it to the final filename
             os.rename(os.path.join(target_dir, filename_tmp), os.path.join(target_dir, filename))
+            print(f"{title}\t{Fore.GREEN}DOWNLOADED{Style.RESET_ALL}")
 
         except Exception as e:
-            print(f"{Fore.RED}Download failed: {work.title}\t\t{Style.DIM}{e.args[0]}{Style.RESET_ALL}")
+            print(f"{title}\t{Fore.RED}FAILED\t\t{Style.DIM}{e.args[0]}{Style.RESET_ALL}")
 
-    print(f"{Fore.GREEN}Download complete!{Style.RESET_ALL}")
+    print(f"{Fore.MAGENTA}{Style.BRIGHT}Download complete!{Style.RESET_ALL}")
 
 
 # Main function
@@ -69,8 +73,20 @@ def main():
 
     download_subscriptions(session, TARGET_DIR)
 
+    # Set the timer to 24 hours
+    timerInSeconds = 60*60*24
+
+    # Print the Date and Time of the next run
+    # Display in the format "YYYY-MM-DD HH:MM:SS"
+    # Get current time in seconds and add the timerInSeconds
+
+    now = datetime.now()
+    nextRun = now + timedelta(seconds=timerInSeconds)
+
+    print(f"{Fore.MAGENTA}{Style.BRIGHT}Next run: {Style.RESET_ALL}{nextRun.strftime('%Y-%m-%d %H:%M:%S')}\n\n")
+
     # Schedule the main function to run again in 24 hours
-    threading.Timer(60*60*24, main).start()
+    threading.Timer(timerInSeconds, main).start()
 
 
 main()
