774591ea824b3ff8c9c966ca69af882ceb95a62d
Joshua Nestler <development@nekanu.com>
2024-04-09 20:44:08 +0200

    Improve resiliency of download and API calls

--- app.py
+++ app.py
@@ -20,31 +20,26 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
 
     for subscription in session.get_work_subscriptions():
 
-        work = AO3.Work(subscription.id, session=session, load_chapters=False)
-        title = f"{Fore.CYAN}{work.authors[0].username} - {work.title}".ljust(80)
-
-        # Skip the download if the work is restricted
-        if work.restricted:
-            print(f"{title}\t{Fore.YELLOW}SKIPPED - RESTRICTED{Style.RESET_ALL}")
-            continue
-
-        filename = f"{work.authors[0].username}_{slugify(work.title)}.{BOOK_FORMAT.lower()}"
-        filename_tmp = filename + ".tmp"
-
-        # Check if the file already exists in the EBOKS_PATH with the same number of chapters
-        if os.path.exists(os.path.join(target_dir, filename)):
-            try:
-                existing_work = epub.read_epub(os.path.join(target_dir, filename))
-                # If the file has the same number of chapters or less, skip the download
-                # (the -3 is to account for the 3 extra chapters in the epub file created by AO3)
-                if work.nchapters <= len(existing_work.spine) - 3:
-                    print(f"{title}\t{Fore.GREEN}SKIPPED - UP TO DATE{Style.RESET_ALL}")
-                    continue
-            except Exception as e:
-                print(f"{Fore.RED}Error reading existing file: {filename}\t\t{Style.DIM}{e.args[0]}{Style.RESET_ALL}")
-
         try:
-            # Download the work
+            work = AO3.Work(subscription.id, session=session, load_chapters=False)
+            title = f"{Fore.CYAN}{subscription.authors[0].username} - {subscription.title}".ljust(80)
+
+            # Skip the download if the work is restricted
+            if work.restricted:
+                print(f"{title}\t{Fore.YELLOW}SKIPPED - RESTRICTED{Style.RESET_ALL}")
+                continue
+
+            filename = f"{subscription.authors[0].username}_{slugify(subscription.title)}.{BOOK_FORMAT.lower()}"
+            filename_tmp = filename + ".tmp"
+
+            # Check if the file already exists in the EBOKS_PATH with the same number of chapters
+            if not work_has_changed(work, os.path.join(target_dir, filename)):
+                print(f"{title}\t{Fore.GREEN}SKIPPED - UP TO DATE{Style.RESET_ALL}")
+                continue
+
+            # Remove any remains of TMP files and download the work
+            if os.path.exists(os.path.join(target_dir, filename_tmp)):
+                os.remove(os.path.join(target_dir, filename_tmp))
             work.download_to_file(os.path.join(target_dir, filename_tmp), BOOK_FORMAT)
             
             # If the file is empty, download failed
@@ -53,7 +48,9 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
                 os.remove(os.path.join(target_dir, filename_tmp))
                 continue
             
-            # If the file is not empty, rename it to the final filename
+            # If the file is not empty, remove old file and rename the new file accordingly
+            if os.path.exists(os.path.join(target_dir, filename)):
+                os.remove(os.path.join(target_dir, filename))
             os.rename(os.path.join(target_dir, filename_tmp), os.path.join(target_dir, filename))
             print(f"{title}\t{Fore.GREEN}DOWNLOADED{Style.RESET_ALL}")
 
@@ -62,6 +59,24 @@ def download_subscriptions(session: AO3.Session, target_dir: str):
 
     print(f"{Fore.MAGENTA}{Style.BRIGHT}Download complete!{Style.RESET_ALL}")
 
+
+def work_has_changed(work: AO3.Work, filepath: str) -> bool:
+    try:
+        # Test if there is even a local file to compare to
+        if not os.path.exists(filepath):
+            return True
+        
+        # Test if the file changed date is older than the work's last updated date
+        if os.path.getmtime(filepath) < work.updated:
+            return True
+        
+        # Test if the file has less chapters than the work
+        existing_work = epub.read_epub(filepath)
+        return work.nchapters > len(existing_work.spine) - 3
+    
+    except Exception as e:
+        print(f"{Fore.RED}Error reading existing file: {filepath}\t\t{Style.DIM}{e.args[0]}{Style.RESET_ALL}")
+        return False
 
 # Main function
 def main():
